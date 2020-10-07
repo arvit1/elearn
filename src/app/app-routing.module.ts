63abfd750382bb77d5c209f14077cb0f1cc635ab
Arvit Varfaj <avarfaj86@gmail.com>
2020-10-07 10:57:02 +0200

    dist removed docs added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { CourseDetailPageComponent } from './pages/course-detail-page/course-det
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: 'landing', component: LandingPageComponent },
+  // { path: 'landing', component: LandingPageComponent },
   { path: 'search', component: SearchPageComponent },
   { path: 'search/category/:id', component: SearchPageComponent },
   { path: 'course/:id', component: CourseDetailPageComponent },
-  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '', component: LandingPageComponent , pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
